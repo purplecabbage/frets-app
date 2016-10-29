7fbfebc6776a8b2614a9580c2d2c02ba03b776c5
Jesse MacFadyen <purplecabbage@gmail.com>
2016-10-28 22:33:20 -0700

    wanna be startin something

---
+++ app/www/js/chords.js
@@ -0,0 +1,393 @@
+var ChordData = {
+    // note names in 'sharp' namings
+    SharpNoteNames: ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
+    // note names in 'flat' namings
+    FlatNoteNames: ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"],
+    // default open strings, indexed into the above note names
+    Tuning: [4, 9, 2, 7, 11, 4],
+    // E2 is the lowest standard tuning guitar note aka Midi# 40
+    MidiRoots: [40, 45, 50, 55, 59, 64],
+
+    ChordTypes: [{
+        abbrev: "maj",
+        title: "Major",
+        shapes: [{
+            root: 0,
+            // Low E String
+            frets: "0,2,2,1,0,0"
+        }, {
+            root: 1,
+            frets: "-1,0,2,2,2,0"
+        }, {
+            root: 3,
+            frets: "-1,-1,0,2,3,2"
+        }, {
+            root: 4,
+            frets: "3,2,0,0,0,3"
+        }, {
+            root: 5,
+            frets: "-11,3,2,0,1,0"
+        }]
+    }, {
+        abbrev: "min",
+        title: "Minor",
+        shapes: [{
+            root: 0,
+            // Low E String
+            frets: "0,2,2,0,0,0"
+        }, {
+            root: 1,
+            frets: "-1,0,2,2,1,0"
+        }, {
+            root: 3,
+            frets: "-1,-1,0,2,3,1"
+        }, {
+            root: 4,
+            frets: "3,1,0,0,3,3"
+        }, {
+            root: 5,
+            frets: "-1,3,2,0,2,0"
+        }]
+    }, {
+        abbrev: "aug",
+        title: "Augmented"
+    }, {
+        abbrev: "sus4",
+        title: "Suspended 4th"
+    }, {
+        abbrev: "6",
+        title: "6th"
+    }, {
+        abbrev: "min6",
+        title: "Minor 6th"
+    }, {
+        abbrev: "7",
+        title: "Dominant 7th"
+    }, {
+        abbrev: "7sus4",
+        title: "Dom 7th Suspended 4th"
+    }, {
+        abbrev: "maj7",
+        title: "Major 7th"
+    }, {
+        abbrev: "min7",
+        title: "Minor 7th"
+    }, {
+        abbrev: "dim7",
+        title: "Diminished 7th"
+    }, {
+        abbrev: "min7b5",
+        title: "Minor 7 flat 5"
+    }, {
+        abbrev: "maj7b5",
+        title: "Major 7 flat 5"
+    }, {
+        abbrev: "min/maj7",
+        title: "Minor / Major 7th"
+    }, {
+        abbrev: "7#5",
+        title: "Dominant 7 sharp 5"
+    }, {
+        abbrev: "7b5",
+        title: "Dominant 7 flat 5"
+    }, {
+        abbrev: "7b9",
+        title: "Dominant 7 flat 9"
+    }, {
+        abbrev: "7#9",
+        title: "Dominant 7 sharp 9"
+    }, {
+        abbrev: "9",
+        title: "9th"
+    }, {
+        abbrev: "add9",
+        title: "add 9"
+    }, {
+        abbrev: "maj9",
+        title: "Major 9"
+    }, {
+        abbrev: "9/6",
+        title: "Major 9 / 6"
+    }, {
+        abbrev: "min9",
+        title: "Minor 9"
+    }, {
+        abbrev: "min9/6",
+        title: "Minor 9 / 6"
+    }, {
+        abbrev: "11",
+        title: "11th"
+    }, {
+        abbrev: "aug11",
+        title: "Augmented 11th"
+    }, {
+        abbrev: "m11",
+        title: "Minor 11th"
+    }, {
+        abbrev: "13",
+        title: "13th"
+    }, {
+        abbrev: "maj13",
+        title: "Major 13th"
+    }, {
+        abbrev: "13b9",
+        title: "13 flat 9"
+    }
+
+    ],
+
+    // A, Am, A aug, A sus4, A6, Am6, A7, A7sus4, Amaj7, Am7, Adim7, Am7b5,
+    Chords: [
+    {
+        name: "A",
+        type: "Open",
+        rt: 0,
+        fret: "-1,0,2,2,2,0",
+        fingers: "0,0,1,2,3,0"
+    },
+    {
+        name: "Am",
+        type: "Open",
+        rt: 0,
+        fret: "-1,0,2,2,1,0",
+        fingers: "0,0,2,3,1,0"
+    },
+    {
+        name: "A aug",
+        type: "Open",
+        rt: 0,
+        fret: "-1,0,2,2,2,1",
+        fingers: "0,0,2,3,4,1"
+    },
+    {
+        name: "A sus4",
+        type: "Open",
+        rt: 0,
+        fret: "-1,0,2,2,3,0",
+        fingers: "0,0,2,2,4,0"
+    },
+    //////
+    {
+        name: "B7",
+        type: "Open",
+        rt: 0,
+        fret: "-1,2,1,2,0,2",
+        fingers: "0,2,1,3,0,4"
+    },
+    {
+        name: "B7sus4",
+        type: "Open",
+        rt: 0,
+        fret: "-1,2,2,2,0,2",
+        fingers: "0,2,1,3,0,4"
+    },
+    {
+        name: "Bm",
+        type: "Open",
+        rt: 0,
+        fret: "-1,2,4,4,3,2",
+        fingers: "0,1,3,4,2,1"
+    },
+    ////////
+    // C, Cm, Caug, Csus4, C6, C7, C7sus4, Cmaj7, Cdim7, Cmaj7-5, Cm/maj7, C9, Cadd9, C9/6
+    {
+        name: "C",
+        type: "Open",
+        rt: 0,
+        fret: "-1,3,2,0,1,0",
+        fingers: "0,3,2,0,1,0"
+    },
+    {
+        name: "Cm",
+        type: "Open",
+        rt: 0,
+        fret: "-1,-1,1,0,1,3",
+        fingers: "0,0,1,0,2,4"
+    },
+    {
+        name: "Caug",
+        type: "Open",
+        rt: 0,
+        fret: "-1,3,2,1,1,0",
+        fingers: "0,3,2,1,1,0"
+    },
+    ///////
+    // D, Dm, Daug, Dsus4, D6, Dm6, D7
+    {
+        name: "D",
+        type: "Open",
+        rt: 0,
+        fret: "-1,-1,0,2,3,2",
+        fingers: "0,0,0,1,3,2"
+    },
+    {
+        name: "Dm",
+        type: "Open",
+        rt: 0,
+        fret: "-1,-1,0,2,3,1",
+        fingers: "0,0,0,2,3,1"
+    },
+    {
+        name: "D7",
+        type: "Open",
+        rt: 0,
+        fret: "-1,-1,0,2,1,2",
+        fingers: "0,0,0,2,1,3"
+    },
+    ////
+    // E, Em, Eaug, Esus4, E6, Em6, E7
+    {
+        name: "E",
+        type: "Open",
+        rt: 0,
+        fret: "0,2,2,1,0,0",
+        fingers: "0,3,2,1,0,0"
+    },
+    {
+        name: "Em",
+        type: "Open",
+        rt: 0,
+        fret: "0,2,2,0,0,0",
+        fingers: "0,3,2,0,0,0"
+    },
+    {
+        name: "Eaug",
+        type: "Open",
+        rt: 0,
+        fret: "0,2,2,1,1,0",
+        fingers:"0,2,3,1,1,0"
+    },
+    {
+        name: "Esus4",
+        type: "Open",
+        rt: 0,
+        fret: "0,2,2,2,0,0",
+        fingers: "0,2,3,4,0,0"
+    },
+    {
+        name: "E6",
+        type: "Open",
+        rt: 0,
+        fret: "0,2,2,1,2,0",
+        fingers: "0,2,3,1,4,0"
+    },
+    {
+        name: "Em6",
+        type: "Open",
+        rt: 0,
+        fret: "0,2,2,0,2,0",
+        fingers: "0,2,3,0,4,0"
+    },
+    {
+        name: "E7",
+        type: "Open",
+        rt: 0,
+        fret: "0,2,0,1,0,0",
+        fingers: "0,3,0,1,0,0"
+    },
+    ////
+    // F, Fmaj7, Fmaj7-5, F7-5, F9, Fadd9, Fmaj9, Fm9
+    {
+        name: "F",
+        type: "Open",
+        rt: 0,
+        fret: "1,3,3,2,1,1",
+        fingers: "1,3,4,2,1,1"
+    },
+    {
+        name: "Fmaj7",
+        type: "Open",
+        rt: 0,
+        fret: "-1,-1,3,2,1,0",
+        fingers: "0,0,3,2,1,0"
+    },
+    {
+        name: "Fmaj7-5",
+        type: "Open",
+        rt: 0,
+        fret: "-1,-1,3,2,0,0",
+        fingers: "0,0,3,2,0,0"
+    },
+    ////
+    // G. Gm, Gaug, Gsus4, G6, Gm6, G7
+    {
+        name: "G",
+        type: "Open",
+        rt: 0,
+        fret: "3,2,0,0,0,3",
+        fingers: "2,1,0,0,0,3"
+    },
+    {
+        name: "Gm",
+        type: "Open",
+        rt: 0,
+        fret: "3,-1,0,3,3,3",
+        fingers: "1,0,0,2,3,4"
+    },
+    {
+        name: "Gaug",
+        type: "Open",
+        rt: 0,
+        fret: "3,2,1,0,0,3",
+        fingers: "3,2,1,0,0,4"
+    },
+    {
+        name: "Gsus4",
+        type: "Open",
+        rt: 0,
+        fret: "3,-1,0,0,1,3",
+        fingers: "3,0,0,0,1,4"
+
+    },
+    ],
+
+
+    Kords: [{
+        base: "A"
+    }, {
+        base: "A#"
+    }, {
+        base: "B"
+    }, {
+        base: "C"
+    }, {
+        base: "C#"
+    }, {
+        base: "D"
+    }, {
+        base: "D#"
+    }, {
+        base: "E"
+    }, {
+        base: "F"
+    }, {
+        base: "F#"
+    }, {
+        base: "G"
+    }, {
+        base: "G#"
+    }
+
+    ],
+
+    getNoteName: function(stringNum, fret, bSharpNotation) {
+        if (fret < 0) {
+            return "x";
+        }
+        return this.SharpNoteNames[(this.Tuning[stringNum] + fret) % 12];
+    },
+
+    getNotesForChord:function(chord) {
+
+        var fretNums = chord.fret.split(",");
+        var noteNames = [];
+
+        for(var n = 0; n < fretNums.length; n++) {
+
+            noteNames.push(this.getNoteName(n,chord.rt + fretNums[n]));
+        }
+
+        return noteNames;
+    }
+
+}
---
+++ app/www/js/fretsApp.js
@@ -0,0 +1,357 @@
+
+// these values are now dynamically computed based on screen size
+var gutter = 35;
+var stringSpace = 50;
+var fretSpace = 60;
+var headerGap = 80;
+
+var noteStrokeClr = "#000";
+var noteFillClr = "#333";
+var noteStrokeWidth = 3;
+var noteLabelTextClr = "#f8f8f8";
+var noteLabelFontWidth = "20px";
+var noteLabelFontWeight = "bold";
+var gFretMarkerStrokeColor = "#FFF";//rgba(0,0,0,1.0)";
+var gFretMarkerFillColor = "#CCC";//rgba(16,16,16,0.5)";
+
+var currentIndex = -1;
+var chordCount;
+var chordSource;
+
+var piano; //Tuned Instrument
+
+var tapEventFunction = ('ontouchstart' in window) ? "touchstart" : "click";
+
+var nativeBridge;
+var paper;
+
+var myApp = new Framework7({
+    animateNavBackIcon:true
+});
+
+Array.max = function() {
+  return Math.max.apply(null, this);
+};
+
+Array.min = function() {
+  return Math.min.apply(null, this);
+};
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add main View
+var mainView = myApp.addView('.view-main', {
+    // Enable dynamic Navbar
+    dynamicNavbar: true,
+    // Enable Dom Cache so we can use all inline pages
+    domCache: true
+});
+
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+    chordSource = ChordData.Chords;
+    initMenu();
+    initRenderBounds();
+    renderChord(localStorage.lastChord || 0);
+    $$('#li_' + localStorage.lastChord).addClass('selected');
+
+    piano = new TunedInstrument();
+    piano.loadVoice("sounds/AccGuitar.wav",48);
+}
+
+function addTapHandler(elem,handler) {
+    elem[tapEventFunction](handler);
+}
+
+function elemToNoteData() {
+     var noteNumber = ChordData.MidiRoots[this.data('stringNum')] + this.data('fretNum');
+     playNote(noteNumber);
+}
+
+function playNote(noteNumber) {
+    // TODO: actually play the note ...
+    console.log("noteNumber = " + noteNumber);
+    piano.noteOn(noteNumber);
+}
+
+
+function initRenderBounds() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    /* Defaults from back in the 320 days
+        var gutter = 35;
+        var stringSpace = 50;
+        var fretSpace = 60;
+        var headerGap = 80;
+    */
+    // calc space between strings
+    stringSpace = Math.floor(w / 7);
+    // calc space between frets
+    fretSpace = Math.floor(stringSpace * 1.4 );
+    gutter = Math.round((w - ((stringSpace ) * 5)) / 2);
+    headerGap = Math.floor(fretSpace * 1.25);
+}
+
+var firstRun = 1;
+$$('.open-panel').on('click', function (e) {
+    // 'left' position to open Left panel
+    if(firstRun) {
+        setTimeout(function(){
+            var elem = document.getElementById('li_' + ( localStorage.lastChord > 10 ?
+            localStorage.lastChord - 10 : localStorage.lastChord));
+            elem.scrollIntoView();
+        },100);
+        firstRun = 0;
+    }
+});
+
+function onChordJump(evt) {
+    var id = evt.target.id ||
+             evt.target.parentNode.id ||
+             evt.target.parentNode.parentNode.id;
+    var chord = id.split("_")[1];
+    myApp.closeModal('.popup-notes');
+    myApp.openPanel("left");
+    document.getElementById("hdr_" + chord).scrollIntoView();
+}
+
+function onMenuItem(evt) {
+    // could be nested child target ... up to 3 deep
+    var id = evt.target.id ||
+             evt.target.parentNode.id ||
+             evt.target.parentNode.parentNode.id;
+
+    var split = id.split("_");
+    var prefix = split[0];
+    var index = parseInt(split[1]);
+
+    if(prefix == "hdr") {
+        // chord header?
+        myApp.popup('.popup-notes');
+    }
+    else {
+        // an acutal chord
+        renderChord(index);
+        setSelectedChord(id);
+        myApp.closePanel();
+    }
+}
+
+function setSelectedChord(id) {
+    $$("li.item-content.selected").removeClass("selected");
+    $$("#"+id).addClass("selected");
+}
+
+function renderChord(index) {
+
+    if(currentIndex == index) {
+        return;
+    }
+
+    localStorage.lastChord = currentIndex = index;
+    if(currentIndex < 0) {
+        currentIndex = chordCount - 1;
+    }
+    currentIndex = currentIndex % chordCount;
+
+    paper.clear();
+
+    var chord = chordSource[currentIndex];
+    var frets = chord.fret.split(",");
+    var fingers = chord.fingers ? chord.fingers.split(",") : [];
+
+    frets = frets.map(function(elem){
+        return parseInt(elem);
+    });
+    fingers = fingers.map(function(elem){
+        return parseInt(elem);
+    });
+
+    // calculate how many frets we need to draw, min 5
+    var chordSpan = 4;//parseInt(Array.max(frets)) - parseInt(Array.min(frets));
+    if(chordSpan < 4) {
+        chordSpan = 4;
+    }
+
+    // for each string 0-5
+    for(var x = 0; x < 5; x++) {
+        // for each fret
+        for(var y = 0; y < chordSpan; y++) {
+
+            elem = paper.rect( gutter + x * stringSpace,
+                               headerGap + y * fretSpace,
+                               stringSpace, fretSpace, 2);
+
+            elem.attr("stroke", "#FFF");
+            //elem.attr("fill", "rgba(255,255,255,1.0)");
+            elem.attr("stroke-width",4);
+
+            // draw the fingerboard fret markers
+            var fret = chord.rt + y;
+
+            // fret markers from zero based fret values, 3,5,7,9,12,15,17
+            // only if x==2 because marker is in the center 012*345
+            if(x == 2 && [2,4,6,8,14,16,18].indexOf(fret) > -1){
+
+                var circ = paper.circle( gutter + x * stringSpace + (stringSpace / 2),
+                                         headerGap + y * fretSpace + (fretSpace / 2) , 12);
+                    circ.attr("stroke", gFretMarkerStrokeColor);
+                    circ.attr("fill", gFretMarkerFillColor);
+                    circ.attr("stroke-width",noteStrokeWidth);
+
+            }
+            else if(fret == 11) { // 12th fret
+                // markers look like 01*23*45
+                if(x == 1 || x == 3) {
+                    var circ = paper.circle( gutter + x * stringSpace + (stringSpace / 2),
+                                             headerGap + y * fretSpace + (fretSpace / 2) , 12);
+                        circ.attr("stroke", gFretMarkerStrokeColor);
+                        circ.attr("fill", gFretMarkerFillColor);
+                        circ.attr("stroke-width",noteStrokeWidth);
+                }
+            }
+        }
+    }
+
+    var posOffset = chord.rt;
+    if(chord.rt > 0) {
+        posOffset = 1;
+        drawFretPostitionText(paper,chord.rt);
+    }
+    else {
+        drawNut(paper);
+    }
+
+    $$(".navbar-inner > .center").html(chord.name);
+
+    for(var n = 0; n < frets.length; n++) {
+        var fret = frets[n];
+
+        if(posOffset > 0 && fret == -1) {
+            // don't draw it
+            addNote(paper,n,posOffset, "X" ); // needs to be a better color to show up
+        }
+        else {
+            var noteName = ChordData.getNoteName(n,chord.rt + fret,true);
+            var finger = fingers[n];
+            addNote(paper,n,posOffset + fret, finger ? finger : noteName );
+        }
+    }
+}
+
+function drawNut(paper) {
+
+    var elem = paper.rect(gutter,headerGap - 8,
+                          stringSpace * 5, 8, 2);
+
+    elem.attr("stroke", "#FFF");
+    elem.attr("fill", "#CCC");//rgba(255,255,255,0.2)");
+    elem.attr("stroke-width",4);
+
+}
+
+function drawFretPostitionText(paper,posText) {
+    var x = 10;//gutter - stringSpace;
+    var y = headerGap + fretSpace  - fretSpace / 2;
+
+    var txt = paper.text(x , y, posText);
+        txt.attr("font-size",noteLabelFontWidth);
+        txt.attr("font-weight",noteLabelFontWeight);
+        txt.attr("fill", noteLabelTextClr );
+}
+
+function addNote(paper,stringNum,fretNum,numText) {
+    var fret = fretNum > -1 ? fretNum : 0;
+    var x = gutter + stringSpace * stringNum;
+    var y = headerGap + fret * fretSpace  - fretSpace / 2;
+
+    if(numText != "x") {
+
+        var circ = paper.circle( x, y, 16);
+            circ.attr({ "stroke" : noteStrokeClr,
+                        "fill" : fretNum ? noteFillClr : "#FFF",
+                        "stroke-width" : noteStrokeWidth });
+
+            circ.data({ "stringNum" : stringNum,
+                        "fretNum" : fretNum });
+
+            addTapHandler(circ,elemToNoteData);
+
+        var txt = paper.text(x , y, numText);
+            txt.attr({ "font-size" : noteLabelFontWidth,
+                       "font-weight" : noteLabelFontWeight,
+                       "fill" : fret ? noteLabelTextClr : noteFillClr });
+
+            txt.data({ "stringNum" : stringNum,
+                        "fretNum" : fretNum });
+            addTapHandler(txt,elemToNoteData);
+    }
+    else {
+
+        var txt = paper.text(x , y, numText);
+            txt.attr("stroke", noteFillClr);
+            txt.attr("fill", fretNum ? noteStrokeClr : "#FFF");
+            txt.attr("font-size",noteLabelFontWidth);
+            txt.attr("font-weight",noteLabelFontWeight);
+    }
+}
+
+
+
+function initMenu() {
+
+    chordDiv.paper = paper = window.Raphael(chordDiv, window.innerWidth, window.innerHeight);
+
+    // TODO: don't flash the DOM repeatedly
+    var lastNote = "";
+    var listItems = [];
+
+    chordCount = chordSource.length;
+
+    //  <li class="item-content" id="li_32">
+    //     <div class="item-inner">
+    //         <div class="item-title">C#min7</div>
+    //     </div>
+    //  </li>
+    for(var n = 0; n < chordCount; n++) {
+        var li = document.createElement("li");
+            li.className = 'item-content';
+            li.id = "li_" + n;
+        var div = document.createElement("div");
+            div.className = 'item-inner';
+        var title = document.createElement("div");
+            title.className = 'item-title';
+            title.innerText = chordSource[n].name;
+        div.appendChild(title);
+        li.appendChild(div);
+
+        var noteName = getChordName(chordSource[n].name);
+        // detect when we go from A to B, and render a B
+        // <li class="item-divider">Divider title here</li>
+
+        if(noteName != lastNote) {
+           var liTitle = document.createElement("li");
+               liTitle.innerText = noteName;
+               liTitle.className = "item-divider";
+               liTitle.id = "hdr_" + noteName;
+           $$('#chordList').append(liTitle);
+           lastNote = noteName;
+        }
+        $$('#chordList').append(li);
+    }
+    $$('#chordList').on("click",onMenuItem);
+    $$('#chordJump').on("click",onChordJump);
+}
+
+function getChordName(strName) {
+
+    var ret = strName.substr(0,2);
+    if(ret.indexOf("b") == 1 ||
+       ret.indexOf("#") == 1 ) {
+           return ret;
+    }
+    return ret.substr(0,1);
+}
