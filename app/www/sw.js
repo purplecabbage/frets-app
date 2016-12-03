6905335ef3b79a4bfaaf358419b789a2e63395b1
Jesse MacFadyen <purplecabbage@gmail.com>
2016-12-03 00:27:55 -0800

    update cache

--- app/www/sw.js
+++ app/www/sw.js
@@ -1,10 +1,48 @@
 
-self.addEventListener('install', function(event) {
+this.addEventListener('install', function(event) {
   // Perform install steps
   console.log("service worker is installing");
+  event.waitUntil(
+    caches.open('v3').then(function(cache) {
+      return cache.addAll([
+        '/css/',
+        '/css/framework7.ios.colors.css',
+        '/css/framework7.ios.min.css',
+        '/css/framework7.ios.colors.min.css',
+        '/css/fretsApp.css',
+        '/css/framework7.ios.css',
+        '/js/',
+        '/js/TunedInstrument.js',
+        '/js/framework7.js',
+        '/js/fretsApp.js',
+        '/js/chords.js',
+        '/js/framework7.min.js',
+        '/lib/',
+        '/lib/raphael-min.js',
+        '/img/',
+        '/img/adirondack_spruce_5_cup.jpg',
+        '/img/debut_dark.png',
+        '/img/icon192.png',
+        '/img/debut_dark_@2X.png',
+        '/img/icon48.png',
+        '/img/herringbone.png',
+        '/img/icon96.png',
+        '/sounds/',
+        '/sounds/AccGuitar.wav',
+        'index.html'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
+this.addEventListener('activate', function(event) {
   // Perform activate steps
   console.log("service worker is activated");
+});
+
+this.addEventListener('fetch', function(event) {
+  //console.log("fetch : " + event.request);
+  event.respondWith(
+    caches.match(event.request)
+  );
 });
